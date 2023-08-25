c93efc172db41792cbc9abe3a2d09dace30e0ce3
Mike Cooper <nverzn@gmail.com>
2023-08-25 14:06:27 +0300

    Updated test for multi-input policy

--- test.py
+++ test.py
@@ -1,31 +1,44 @@
 import gymnasium as gym
 import arm_reach
+import argparse
 
 from stable_baselines3 import PPO
-from models import Encoder
+from models import CustomCombinedExtractor
 
-encoder_feature_dim = 50
-hidden_dim = 4
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-config = {
-    "policy_type": "CnnPolicy",
-    "total_timesteps": 10_000,
-    "env_id": "ArmReach-v0",
-}
+    parser.add_argument("--weights", default="weights/trained_policy")
+
+    args = parser.parse_args()
+    return args
+
+config = parse_args()
+
+config.policy_type = "MultiInputPolicy"
+config.env_id = "ArmReach-v0"
+config.total_timesteps = 10_000
+
+config.encoder_features_dim = 50
+config.encoder_extra_features_dim = 16
+config.hidden_dim = 4
 
 policy_kwargs = dict(
-    features_extractor_class=Encoder,
+    features_extractor_class=CustomCombinedExtractor,
     features_extractor_kwargs=dict(
-        features_dim = encoder_feature_dim,
+        features_dim = config.encoder_features_dim,
+        extra_features_dim = config.encoder_extra_features_dim,
+        num_layers = 4,
+        num_filters = 32,
     ),
-    net_arch=[hidden_dim, hidden_dim, hidden_dim]
+    net_arch=[config.hidden_dim] * 3
 )
 
-model = PPO(config["policy_type"], config["env_id"], policy_kwargs=policy_kwargs, verbose=1)
+model = PPO(config.policy_type, config.env_id, policy_kwargs=policy_kwargs, verbose=1)
 
 
 env = model.get_env()
-model = PPO.load("weights/trained_policy", env=env)
+model = PPO.load(config.weights, env=env)
 
 obs = env.reset()
 
